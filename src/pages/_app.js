d9aa2871057d0bb02638c98059fd407de8af36fe
chandanneralgi <106388395+chandanneralgi@users.noreply.github.com>
2024-05-19 11:57:03 +0530

    enabled insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
         <AnimatePresence mode="wait">
         <Component key={router.asPath} {...pageProps} />
         </AnimatePresence>
+        <SpeedInsights/>
         <Footer />
       </main>
     </>
